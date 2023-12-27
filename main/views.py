07a8aa2085c52112e460c560b2374a9da89977ff
Oleg Chistyakov <oneofthemoon75@gmail.com>
2023-12-28 00:15:41 +0300

    Django prjct_0.1

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from typing import Any
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+  context: dict[str, Any] = {
+    'title': 'Home',
+    'content': 'Главная страница магазина - HOME',
+    'list': ['first', 'second'],
+    'dict': {'first': 1},
+    'is_authenticated': False
+  }
+
+  return render(request, 'main/index.html', context)
+
+def about(request):
+  return HttpResponse('About page')

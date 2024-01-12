d32a717cc633ef1b038331a9c6e2d56a349110ce
OlegCh <oneofthemoon75@gmail.com>
2024-01-12 21:38:20 +0300

    final code for search released

--- goods/utils.py
+++ goods/utils.py
@@ -14,15 +14,9 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    return Products.objects.filter(description__search=query)
-    
-    
-
-
-
-''' vector = SearchVector("name", "description")
+    vector = SearchVector("name", "description")
     query = SearchQuery(query)
-
+    
     result = (
         Products.objects.annotate(rank=SearchRank(vector, query))
         .filter(rank__gt=0)
@@ -45,7 +39,7 @@ def q_search(query):
             stop_sel="</span>",
         )
     )
-    return result'''
+    return result
     # keywords = [word for word in query.split() if len(word) > 2]
 
     # q_objects = Q()

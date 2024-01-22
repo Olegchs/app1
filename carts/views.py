023b068f75da17c60b8a80b4040163513a12e1b1
OlegCh <oneofthemoon75@gmail.com>
2024-01-23 00:17:52 +0300

    all card templates were included

---
+++ carts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def cart_add(request, producct_id ):
+    ...
+
+def cart_change(request, producct_id ):
+    ...
+
+def cart_remove(request, producct_id ):
+    ...
+
+
